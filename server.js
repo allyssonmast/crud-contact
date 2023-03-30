171561b886c64f84ef59b711a4de84814d9d4055
allyssonmastrangelo <allysson-m@hotmail.com>
2023-03-30 20:56:46 -0300

    connect to mongoDb

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandle");
+const connectDb = require("./config/dbConnection");
+
 
 const dotenv = require("dotenv").config();
+
+connectDb();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use("/api/contacts",require("./routes/contactRoutes"))
+app.use(express.json());
+
+
+app.use("/api/contacts",require("./routes/contactRoutes"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
